00d4b2090e8d282b38073d7cb7aa1ab6d2023ff2
Érico Andrei <erico@simplesconsultoria.com.br>
2013-11-12 09:17:14 -0200

    Small fixes

--- setup.py
+++ setup.py
@@ -43,7 +43,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'apyb.conference',
         'collective.cover',
         'collective.polls',
         'collective.upload',
